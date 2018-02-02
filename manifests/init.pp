d7b30f09fe2928988eb61eee9bf1964b59836725
ju5t <m@ju5t.nl>
2018-02-02 22:38:38 +0100

    Update repo locations for yum and apt

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,25 +1,27 @@
+# r1soft
 class r1soft {
   case $::operatingsystem {
-    redhat, centos: {
+    'redhat', 'centos': {
       yumrepo { 'r1soft':
-        baseurl  => 'http://repo.r1soft.com/yum/stable/$basearch/',
-        descr    => 'R1Soft Server Backup Manager Repository',
-        enabled  => 1,
-        gpgcheck => 0,
-        name     => 'r1soft',
+        baseurl       => 'http://repo.r1soft.com/yum/stable/$basearch/',
+        descr         => 'R1Soft Server Backup Manager Repository',
+        enabled       => 1,
+        name          => 'r1soft',
+        repo_gpgcheck => 1,
+        gpgkey        => 'http://repo.r1soft.com/r1soft.asc',
       }
 
       # Set the right java package, this can be used later on with java_ks
       $java_package = 'java-1.6.0-openjdk-devel'
     }
-    debian, ubuntu: {
+    'debian', 'ubuntu': {
       apt::source { 'r1soft':
         comment     => 'R1Soft Server Backup Manager Repository',
         location    => 'http://repo.r1soft.com/apt',
         release     => 'stable',
         repos       => 'main',
         include_src => false,
-        key         => 'A40384ED',
+        key         => '66BD1D82',
         key_source  => 'http://repo.r1soft.com/r1soft.asc',
       }
 
