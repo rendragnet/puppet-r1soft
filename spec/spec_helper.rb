868e5cadbadad1cc44c95a7969b1533ba20de691
ju5t <m@ju5t.nl>
2018-12-08 10:50:46 +0100

    (#7) Move Hiera tests to spec directory

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,7 +12,7 @@ require 'rspec-puppet-facts'
 include RspecPuppetFacts
 
 RSpec.configure do |c|
-  c.hiera_config = File.expand_path(File.join(__FILE__, '../../hiera.yaml'))
+  c.hiera_config = File.expand_path(File.join(__FILE__, '../../spec/hiera.yaml'))
 end
 
 SimpleCov.start do
